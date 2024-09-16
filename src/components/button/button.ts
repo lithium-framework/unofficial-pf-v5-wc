501b1b4cec2e8f161739c252e82fb619a10b0015
MortallicaXxX <MortallicaXxX@hotmail.com>
2024-09-16 20:32:14 +0200

    init

--- src/components/button/button.ts
+++ src/components/button/button.ts
@@ -1,25 +1,42 @@
-import { html , render , WebComponent , customElement , attr , attrState , state, css } from '@lithium-framework/core';
-import '@patternfly/react-core/dist/styles/base.css';
+import { html , WebComponent , customElement , attr , attrState , state, css } from '@lithium-framework/core';
 
-const x = attrState as any
-
+import { BaseStyle } from '../../css/base';
+import ButtonStyles from 'bundle-text:@patternfly/react-styles/css/components/Button/button.css';
 
 @customElement({
   name : 'pf-button',
   template : html`${( button:PfButton ) => {
     return html`<button 
-      class=${`pf-v5-c-button pf-m-${button.variant} ${button.kind ? `pf-m-${button.kind}` : ``} ${!button.disabled || button.disabled == "true" ? `pf-m-aria-disabled` : ``}`} 
+      class=${[
+        'pf-v5-c-button',
+        `pf-m-${button.variant}`,
+        `${button.kind ? `pf-m-${button.kind}` : ``}`,
+        `${ !button.isDisabled ? `` : button.isDisabled || button.isDisabled == true ? `pf-m-aria-disabled` : `` }`
+      ].join(' ')}
       type="button">
-        ${button.text}
+        <slot></slot>
       </button>`
   }}`,
-  shadowOptions: null
+  styles : [ 
+    BaseStyle,
+    css`${ButtonStyles}` 
+  ],
+  shadowOptions: { mode: 'open' }
 })
 export class PfButton extends WebComponent{
 
-  @x() text:string = "coucou";
-  @x() variant: "primary" | "secondary" | "tertiary" = "primary";
-  @x() kind: "warning" | "danger";
-  @x() disabled:"false" | "true" = "false";
+  @attrState() variant: "primary" | "secondary" | "tertiary" = "primary";
+  @attrState() kind: "warning" | "danger";
+  @attrState() disabled:"false" | "true" = null;
+  
+  @state isDisabled = false;
+
+  attributeChangedCallback(name: string, oldValue: string | null, newValue: string | null): void {
+    
+    if( name == 'disabled' ){
+      this.isDisabled = newValue == "true" || newValue == "" ? true : false;
+    }
+
+  }
 
 }
